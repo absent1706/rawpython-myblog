f1573c0fbf46bd54a952d7573dc94bee626867dd
Alexander Litvinenko <litvinenko1706@gmail.com>
2017-02-04 15:36:23 +0200

    +parse GET params

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+from urlparse import parse_qs
+
 def application(environ, start_response):
+    params = parse_qs(environ.get('QUERY_STRING', ''))
+    name = params['name'][0] if 'name' in params else 'Bob'
+
     start_response('200 OK', [('Content-Type', 'text/html')])
-    return ['Hello <b>Bob</b>!']
+    return ['Hello <b>{}</b>'.format(name)]
+
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
--- readme.md
+++ readme.md
@@ -9,3 +9,6 @@ python app.py
 ##### 0.0.1
 Simplest 'Hello world'.
 No dependencies, no auto-reload (re-run app manually to see changes).
+
+##### 0.0.2
+++ Parse GET params
