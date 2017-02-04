3efb09e3815ee47d5b1fdf4dda4eac8a502becd5
Alexander Litvinenko <litvinenko1706@gmail.com>
2017-02-04 18:20:46 +0200

    ~ move App class to standalone module

--- app.py
+++ app.py
@@ -1,43 +1,8 @@
-import re
-import httplib
-from urlparse import parse_qs
-
-class App:
-    def __init__(self, routes):
-        self.routes = routes
-
-    def __call__(self, environ, start_response):
-        url = environ.get('PATH_INFO', '')
-        request = dict(
-            url=url,
-            get=parse_qs(environ.get('QUERY_STRING', '')),
-            environ=environ,
-        )
-
-        matched_route = False
-        for route in self.routes:
-            match = re.match(route['regex'], url)
-            if match:
-                kwargs = match.groupdict()
-                response = route['view'](request, **kwargs)
-                # if resp is just a string, assert 200 code and some headers
-                # otherwise just take it assuming it's a tuple of suggested format
-                code, headers, body = 200, {'Content-Type': 'text/html'}, response\
-                                      if isinstance(response, basestring) \
-                                      else response
-                matched_route = True
-                break
-        if not matched_route:
-            code, headers, body = 404, {'Content-Type': 'text/html'}, \
-                                  '<h1>Sorry, page does not exist</h1>'
-
-        start_response('{} {}'.format(code, httplib.responses[code]),
-                       headers.items())
-        return body
-
+import mywebpy
 
 from views import *
-app = App([
+
+app = mywebpy.App([
     dict(regex=r'^/$', view=index),
     dict(regex=r'^/posts/(?P<id>\d+)$', view=post_detail),
 ])
